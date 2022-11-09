706c827d9aa6d6681d6dbb48dc9a174de4acc3d6
Marcin Bieniek <marcingim@gmail.com>
2022-11-09 12:10:46 +0100

    Add option which deletes img when registration is failed & add slight code corrections

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
-const getImageFileType = require('../utils/getImageFileType')
+const getImageFileType = require('../utils/getImageFileType');
+const fs = require('fs');
 
 exports.register = async (req, res) => {
     try {    
@@ -9,45 +10,76 @@ exports.register = async (req, res) => {
 
         console.log(req.file)
 
-        if (login && typeof login === 'string' && password && typeof password === 'string' && req.file && ['image/png', 'image/jpeg', 'image/gif'].includes(fileType)) {
+        if (login 
+            && typeof login === 'string' 
+            && password 
+            && typeof password === 'string' 
+            && req.file && ['image/png', 'image/jpeg', 'image/gif'].includes(fileType)) {
+            
             const userWithLogin = await User.findOne({ login });
             if (userWithLogin) {
-                return res.status(409).send({ message: 'User with this login already exists'});
+                fs.unlinkSync(req.file.path)
+                return res
+                    .status(409)
+                    .send({ message: 'User with this login already exists'});
             }
-
-            const user = await User.create({ login, password: await bcrypt.hash(password, 10), avatar: req.file.filename});
-            res.status(201).send({ message: "User created" + user.login})
+            const user = await User.create({ 
+                login, 
+                password: await bcrypt.hash(password, 10), 
+                avatar: req.file.filename,
+                telephone });
+            res
+                .status(201)
+                .send({ message: "User created " + user.login})
         } else {
-            res.status(400).send({ message: 'Bad request'})
+            res
+                .status(400)
+                .send({ message: 'Bad request'})
         }
     } catch (err) {
-        res.status(500).send({ message: err.message })
+        res
+            .status(500)
+            .send({ message: err.message })
     }
 }
 
 exports.login = async (req, res) => {
     try {
         const { login, password } = req.body
-        if (login && typeof login === 'string' && password && typeof password === 'string') {
+        if (login 
+            && typeof login === 'string' 
+            && password 
+            && typeof password === 'string') {
+            
             const user = await User.findOne({ login });
             if (!user) {
-                res.status(400).send({ message: 'Login or password are incorrect' });
+                res
+                .status(400)
+                .send({ message: 'Login or password are incorrect' });
             }
             else {
                 if (bcrypt.compareSync(password, user.password)) {
                     req.session.login = user.login;
                     req.session.save();
-                    res.status(200).send({ message: 'Login succesful' });
+                    res
+                        .status(200)
+                        .send({ message: 'Login succesful' });
                 } 
                 else {
-                    res.status(400).send({ message: 'Login or password are incorrect' })
+                    res
+                        .status(400)
+                        .send({ message: 'Login or password are incorrect' })
                 }
             }
         } else {
-            res.status(400).send({ message: 'Bad request' });
+            res
+                .status(400)
+                .send({ message: 'Bad request' });
         }
     } catch (err) {
-        res.status(500).send({ message: err.message });
+        res
+            .status(500)
+            .send({ message: err.message });
     }
 
 }
@@ -59,8 +91,12 @@ exports.getUser = async (req, res) => {
 exports.logout = async (req, res) => {
     try {
         req.session.destroy();
-        return res.status(200).send({ message: 'Session ended' });
+        return res
+            .status(200)
+            .send({ message: 'Session ended' });
     } catch (err) {
-        return res.status(500).send({ message: err.message });
+        return res
+            .status(500)
+            .send({ message: err.message });
     }
 }
