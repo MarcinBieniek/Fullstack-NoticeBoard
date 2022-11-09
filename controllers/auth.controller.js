783ebe48970d297cf045888d58814cb0f0b2ba41
Marcin Bieniek <marcingim@gmail.com>
2022-11-09 01:45:46 +0100

    Add multer & set image support for form data

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,19 @@
-const User = require('../models/user.model')
+const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
+const getImageFileType = require('../utils/getImageFileType')
 
 exports.register = async (req, res) => {
     try {    
         const { login, password } = req.body;
-        if (login && typeof login === 'string' && password && typeof password === 'string') {
+        const fileType = req.file ? await getImageFileType(req.file) : 'unknown';
+
+        if (login && typeof login === 'string' && password && typeof password === 'string' && req.file && ['image/png', 'image/jpeg', 'image/gif'].includes(fileType)) {
             const userWithLogin = await User.findOne({ login });
             if (userWithLogin) {
                 return res.status(409).send({ message: 'User with this login already exists'});
             }
 
-            const user = await User.create({ login, password: await bcrypt.hash(password, 10)});
+            const user = await User.create({ login, password: await bcrypt.hash(password, 10), avatar: req.file.filename});
             res.status(201).send({ message: "User created" + user.login})
         } else {
             res.status(400).send({ message: 'Bad request'})
@@ -54,7 +57,7 @@ exports.getUser = async (req, res) => {
 exports.logout = async (req, res) => {
     try {
         req.session.destroy();
-        return res.status(200).send({ message: 'Session destroyed' });
+        return res.status(200).send({ message: 'Session ended' });
     } catch (err) {
         return res.status(500).send({ message: err.message });
     }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../utils/authMiddleware')
-
+const authMiddleware = require('../utils/authMiddleware');
 const authController = require('../controllers/auth.controller');
+const imageUpload = require('../utils/imageUpload');
 
-router.post('/register', authController.register);
+router.post('/register', imageUpload.single('avatar'), authController.register);
 router.post('/login', authController.login);
 router.get('/user', authMiddleware, authController.getUser)
 router.get('/logout', authMiddleware, authController.logout)
