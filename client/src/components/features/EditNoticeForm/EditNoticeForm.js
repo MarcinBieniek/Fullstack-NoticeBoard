d69b096feaa397fb4ce77a95994a67f056731d35
Marcin Bieniek <marcingim@gmail.com>
2022-11-27 21:26:03 +0100

    Add edit offer alerts and loading spinner

--- client/src/components/features/EditNoticeForm/EditNoticeForm.js
+++ client/src/components/features/EditNoticeForm/EditNoticeForm.js
@@ -6,6 +6,9 @@ import { useSelector } from 'react-redux';
 import { getNoticeById } from '../../../redux/noticesReducer';
 import { Navigate } from 'react-router-dom';
 import { API_URL } from '../../../config';
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Spinner from 'react-bootstrap/Spinner';
 
 // components
 import NoticeForm from '../NoticeForm/NoticeForm';
@@ -14,6 +17,8 @@ const EditNoticeForm = () => {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const [status, setStatus] = useState(null); // null, 'loading, 'success', 'serverError', 'clientError',
 
     const { id } = useParams();
 
@@ -35,10 +40,16 @@ const EditNoticeForm = () => {
             method: 'PUT',
             body: editedData,
         };
+        setStatus('loading');
         fetch(API_URL + 'api/ads/' + id, options)
             .then(res => {
                 if (res.status === 200) {
-                setTimeout(() => navigate('/'), 1000);
+                    setStatus('success');
+                    setTimeout(() => navigate('/'), 2000);
+                } else if (res.status === 400) {
+                    setStatus('clientError');
+                } else {
+                    setStatus('serverError');
                 }
             });
     };
@@ -48,20 +59,49 @@ const EditNoticeForm = () => {
 
     if(!noticeData) return <Navigate to="/" />
     else return (
-        <NoticeForm 
-            action={handleSubmit} 
-            actionText={actionText}
-            title={noticeData.title} 
-            description={noticeData.description}
-            location={noticeData.location} 
-            price={noticeData.price}
-            bedrooms={noticeData.bedrooms}
-            bathrooms={noticeData.bathrooms}
-            rooms={noticeData.rooms}
-            meters={noticeData.meters} 
-            photo={noticeData.photo} 
-            user={noticeData.user}
-        />
+        <div>
+            {status === 'success' && (
+                <Alert variant='success'>
+                    <Alert.Heading>Success!</Alert.Heading>
+                    <p>Offer edited succesfully. You'll be redirected to main page.</p>
+                </Alert>
+            )}
+  
+            {status === 'serverError' && (
+                <Alert variant='danger'>
+                    <Alert.Heading>Something went wrong...</Alert.Heading>
+                    <p>Unexpected error.. Please try again!</p>
+                </Alert>
+            )}
+  
+            {status === 'clientError' && (
+                <Alert variant='danger'>
+                    <Alert.Heading>Not enough data</Alert.Heading>
+                    <p>Please fill empty fields</p>
+                </Alert>
+            )}
+  
+            {status === 'loading' && (
+                <Spinner animation='border' role='status' className='d-block mx-auto'>
+                    <span className='visually-hidden'>Loading...</span>
+                </Spinner>
+            )}
+
+            <NoticeForm 
+                action={handleSubmit} 
+                actionText={actionText}
+                title={noticeData.title} 
+                description={noticeData.description}
+                location={noticeData.location} 
+                price={noticeData.price}
+                bedrooms={noticeData.bedrooms}
+                bathrooms={noticeData.bathrooms}
+                rooms={noticeData.rooms}
+                meters={noticeData.meters} 
+                photo={noticeData.photo} 
+                user={noticeData.user}
+            />
+        </div>
     )
 }
 
