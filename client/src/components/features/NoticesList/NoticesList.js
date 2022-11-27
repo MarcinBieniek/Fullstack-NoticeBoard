a92f4603c635050db487def0bd4b6de244b3464f
Marcin Bieniek <marcingim@gmail.com>
2022-11-27 21:53:30 +0100

    Add loading spinner on main site

--- client/src/components/features/NoticesList/NoticesList.js
+++ client/src/components/features/NoticesList/NoticesList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import {getAllNotices} from '../../../redux/noticesReducer';
 import Row from 'react-bootstrap/Row';
+import Spinner from 'react-bootstrap/Spinner';
 
 import NoticeHeader from '../NoticeHeader/NoticeHeader';
 
@@ -9,6 +10,14 @@ const NoticesList = () => {
   
     const notices = useSelector(state => getAllNotices(state));
 
+    if (!notices) {
+        return (
+            <Spinner animation='border' role='status' className='d-block mx-auto'>
+                <span>Loading...</span>
+            </Spinner>
+        );
+    }
+ 
     return (
         <main>
             <Row xs={1} md={3} className="justify-content-space-between">
