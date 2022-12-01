6bbc19c92dc625f916a28b1da800d8e9755f3861
Marcin Bieniek <marcingim@gmail.com>
2022-12-01 19:32:34 +0100

    Heroku test 9

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 export const API_URL =
   process.env.NODE_ENV === 'production' 
-    ? '' 
-    : 'http://localhost:8000/';
+    ? '/api' 
+    : 'http://localhost:8000/api';
 
 export const IMAGES_URL =
   process.env.NODE_ENV === 'production'
