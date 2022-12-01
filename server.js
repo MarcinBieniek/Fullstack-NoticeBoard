fe295fc634f5a1d2c7c7dfad496caa8fee42615c
Marcin Bieniek <marcingim@gmail.com>
2022-12-01 21:49:39 +0100

    COrs fix

--- server.js
+++ server.js
@@ -36,14 +36,7 @@ db.on('error', err => console.log('Error ' + err));
 // add middleware
 if(process.env.NODE_ENV !== 'production') {
   app.use(
-    cors({
-      origin: [
-      'http://localhost:3000',
-      'http://localhost:8000',
-      'https://real-estate-offers-mb.herokuapp.com',
-      ],
-      credentials: true,
-    })
+    cors()
   );
 };
 app.use(express.json());
