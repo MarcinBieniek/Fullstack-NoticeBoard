eea858ff6226e9285fd37ce281e6d95488f5a844
Marcin Bieniek <marcingim@gmail.com>
2023-04-14 18:45:37 +0200

    Fix server

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const cors = require('cors');
-const session = require('express-session');
 const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcryptjs');
 
 // set dotenv
 dotenv.config();
 
-// start express server
-const app = express(); 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// import routes
+const noticesRoutes = require('./routes/notices.routes');
+const authRoutes = require('./routes/auth.routes');
 
-// connect to db 
-mongoose
-  .connect(process.env.DB_URL, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
-.then(() => {console.log('DB connection succesfull')})
-.catch((err) => {console.log('DB error is', err)});
+const app = express();
 
-// add middleware
-app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:8000"],
-  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
-  credentials: true
-}))
-if(process.env.NODE_ENV !== 'production') {
-  app.use(
-    cors({
-      origin: ['http://localhost:3000'],
-      credentials: true,
-    })
-  );
-};
+/* MIDDLEWARE */
+
+/// WIP
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(session({ 
@@ -57,21 +37,41 @@ app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/uploads/')));
 
-// import routes
-const noticesRoutes = require('./routes/notices.routes');
-const authRoutes = require('./routes/auth.routes');
-
 // use routes
 app.use('/api', noticesRoutes);
 app.use('/auth', authRoutes);
 
+// error status
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
+
+// React website
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-// error status
-app.use((req, res) => {
-    res.status(404).send('404 not found...');
-})
+///WIP
 
+/* MONGOOSE */
+// connects our backend code with the database
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+const password = process.env.PASSDB;
 
+if(NODE_ENV === 'production') dbUri = 'mongodb+srv://MarcinEden:' + password + '@cluster1.sg1w6lh.mongodb.net/RealEstateBoard?retryWrites=true&w=majority';
+else dbUri = 'mongodb+srv://MarcinEden:' + password + '@cluster1.sg1w6lh.mongodb.net/RealEstateBoard?retryWrites=true&w=majority';
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Successfully connected to the database');
+});
+db.on('error', err => console.log('Error: ' + err));
+
+/* START SERVER */
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log('Server is running on port: '+ port);
+});
